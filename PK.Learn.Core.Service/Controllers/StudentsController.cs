80a5c6a06252e02c7dfc5700b411b28380cb8599
Pranavkumar <pranav.kumar.ext@siemens.com>
2025-07-28 21:53:09 +0530

    Created the Controller

---
+++ PK.Learn.Core.Service/Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PK.Learn.Core.Service.Models;
+
+namespace PK.Learn.Core.Service.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public void GetStudentById(int id,string name)
+        {
+
+
+        }
+    }
+}
---
+++ PK.Learn.Core.Service/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿namespace PK.Learn.Core.Service.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        public string Name { get; set; }
+
+        public int MajorId { get; set; }
+
+        public int Age { get; set; }
+
+        public string MobileNo { get; set; }
+
+        public string EmailId { get; set; }
+
+        public DateTime Dob { get; set; }
+    }
+}
